089311a7e726436ce0b52683fcfb02d986410ca9
koheitakahashi <t.NMP300@gmail.com>
2020-06-20 15:29:59 +0900

    Suicaのチャージ機能を追加

--- lib/suica.rb
+++ lib/suica.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class Suica
+  attr_accessor :deposit
+
+  def initialize
+    @deposit = 0
+  end
+
+  def charge(amount)
+    @deposit += amount if amount >= 100
+    @deposit
+  end
 end
--- test/suica_test.rb
+++ test/suica_test.rb
@@ -1,17 +1,33 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require './lib/suica'
 
 class SuicaTest < Minitest::Test
-  def test_step_0_Suicaのチャージ
-    # 以下の2行は動作確認用のコードなので削除して良い
-    suica = Suica.new
-    assert !suica.nil?
+  # def test_step_0_Suicaのチャージ
+  # 以下の要求仕様（テストケース）は必要に応じてテストメソッド（def test_xxx）を分けても良い
+  #
+  # Suicaには100円以上の任意の金額をチャージできる
+  # Suicaは現在のチャージ残高を取得できる。
+  # end
 
-    # 以下の要求仕様（テストケース）は必要に応じてテストメソッド（def test_xxx）を分けても良い
-    #
-    # Suicaには100円以上の任意の金額をチャージできる
-    # Suicaは現在のチャージ残高を取得できる。
+  def setup
+    @suica = Suica.new
   end
 
+  def test_step_0_Suicaには100円以上の金額をチャージできる
+    assert_equal(0, @suica.charge(10))
+    assert_equal 100, @suica.charge(100)
+  end
+
+  def test_step_0_金額をチャージしたときチャージ残高を取得できる
+    assert_equal(0, @suica.deposit)
+  end
+
+  def test_step_0_金額をチャージしたときチャージ残高を取得できる
+    @suica.charge(200)
+
+    assert_equal(200, @suica.deposit)
+  end
   # 以下、step1以降の要求仕様も同様にTDDで自動販売機プログラムを書いていく
 end
