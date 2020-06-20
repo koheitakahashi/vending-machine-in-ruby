18b6890b031e5d03fefe4e667a7db30b76027b84
koheitakahashi <t.NMP300@gmail.com>
2020-06-20 17:53:32 +0900

    ジュースを購入する機能を追加

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 class VendingMachine
-  attr_accessor :drink_stocks
+  attr_accessor :drink_stocks, :sales_amount
 
   def initialize
     @drink_stocks = [{ price: 120, name: 'コーラ', stock: 5 }]
+    @sales_amount = 0
   end
 
   def first_drink_stocks
     @drink_stocks.first
+  end
+
+  def purchase(suica)
+    return "チャージ残高が足りません" if suica.deposit < first_drink_stocks[:price]
+
+    return "在庫がありません" if first_drink_stocks[:stock] === 0
+
+    suica.deposit -= first_drink_stocks[:price]
+    first_drink_stocks[:stock] -= 1
+    @sales_amount += first_drink_stocks[:price]
   end
 end
--- test/vending_machine_test.rb
+++ test/vending_machine_test.rb
@@ -2,6 +2,7 @@
 
 require 'minitest/autorun'
 require './lib/vending_machine'
+require './lib/suica'
 
 class VendingMachineTest < Minitest::Test
   # def test_step_1_ジュースの管理
@@ -23,4 +24,40 @@ class VendingMachineTest < Minitest::Test
   end
 
   # 以下、step2以降の要求仕様も同様にTDDで自動販売機プログラムを書いていく
+  def test_step_2_現在の売上金額を取得できる
+    machine = VendingMachine.new
+
+    assert_equal 0, machine.sales_amount
+  end
+
+  def test_step_2_チャージ残高が足りない場合、購入操作を行っても何もしない。
+    suica = Suica.new
+    suica.charge(0)
+
+    machine = VendingMachine.new
+
+    assert_equal("チャージ残高が足りません", machine.purchase(suica))
+  end
+
+  def test_step_2_在庫がない場合、購入操作を行っても何もしない。
+    suica = Suica.new
+    suica.charge(120)
+
+    machine = VendingMachine.new
+    machine.first_drink_stocks[:stock] = 0
+
+    assert_equal("在庫がありません", machine.purchase(suica))
+  end
+
+  def test_step_2_ジュース値段以上のチャージ残高がある条件下で購入すると、自動販売機はジュースの在庫を減らし、売り上げ金額を増やし、Suicaのチャージ残高を減らす。
+    suica = Suica.new
+    suica.charge(120)
+
+    machine = VendingMachine.new
+    machine.purchase(suica)
+
+    assert_equal(0, suica.deposit)
+    assert_equal(4, machine.first_drink_stocks[:stock])
+    assert_equal(120, machine.sales_amount)
+  end
 end
